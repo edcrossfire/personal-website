cdd40d15cb776c64b0d41ee97b0264b1fa5ad717
edcrossfire <ed@crossfire.network>
2022-07-07 19:49:17 -0400

    Added telegram and crossfire

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Image from 'next/image';
 import SCREENSHOT1 from "../public/images/portfolio-screenshot.jpg";
 import SCREENSHOT2 from "../public/images/solanapay-shop.jpg";
+import SCREENSHOT3 from "../public/images/algo-vote-screenshot.jpg";
+import SCREENSHOT4 from "../public/images/crossfire-screenshot.jpg";
 import {FaGithub} from "react-icons/fa";
 import {CgWebsite} from "react-icons/cg";
 
@@ -19,11 +21,11 @@ const Portfolio = () => {
             <div className="shadow-lg w-full h-40 rounded-md">
                 <div className="flex justify-between col w-full h-full">
                     <div className="flex w-1/3 shadow-sm">
-                        <Image className="object-cover h-full rounded-l-md" src={SCREENSHOT1} alt="Bonnie Avatar" />
+                        <Image className="object-cover h-full rounded-l-md" src={SCREENSHOT1} alt="screenshot" />
                     </div>
                     <div className="flex w-2/3 pl-3 pt-2 pb-2 pr-3 flex-col justify-center text-sm h-full">
                         <h3 className="font-Primary text-lg font-bold">This Website!</h3>
-                        <h4 className="font-Secondary text-xs font-bold">NextJS | Tailwind CSS | HTML</h4>
+                        <h4 className="font-Secondary text-xs font-bold">NextJS | Tailwind CSS</h4>
                         <p className="font-Secondary text-sm pt-1 pb-1 lg:pr-4 leading-tight">My own portfolio website that you&apos;re viewing now. Built using NextJS and designed with Tailwind CSS.</p>
                         <ul className="flex space-x-3 pt-0.5">
                             <li><a href="https://github.com/edcrossfire/" target="_blank" rel="noreferrer"><FaGithub size={20} color="#BF6330"/></a></li>
@@ -35,15 +37,48 @@ const Portfolio = () => {
             <div className="shadow-lg w-full h-40 rounded-md">
                 <div className="flex justify-between col w-full h-full">
                     <div className="flex w-1/3 shadow-sm">
-                        <Image className="object-cover h-full rounded-l-md" src={SCREENSHOT2} alt="Bonnie Avatar" />
+                        <Image className="object-cover h-full rounded-l-md" src={SCREENSHOT4} alt="screenshot" />
+                    </div>
+                    <div className="flex w-2/3 pl-3 pt-2 pb-2 pr-3 flex-col justify-center text-sm h-full">
+                        <h3 className="font-Primary text-lg font-bold">Crossfire.Network</h3>
+                        <h4 className="font-Secondary text-xs font-bold">WordPress | WooCommerce | Elementor</h4>
+                        <p className="font-Secondary text-sm pt-1 pb-1 lg:pr-4 leading-tight">An eCommerce platform that modernizes international clients' sales channels.</p>
+                        <ul className="flex space-x-3 pt-0.5">
+                            <li><a href="https://crossfire.network" target="_blank" rel="noreferrer"><CgWebsite size={20} color="#BF6330"/></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="shadow-lg w-full h-40 rounded-md">
+                <div className="flex justify-between col w-full h-full">
+                    <div className="flex w-1/3 shadow-sm">
+                        <Image className="object-cover h-full rounded-l-md" src={SCREENSHOT2} alt="screenshot" />
                     </div>
                     <div className="flex w-2/3 pl-3 pt-2 pb-2 pr-3 flex-col justify-center text-sm h-full">
                         <h3 className="font-Primary text-lg font-bold">Solana Pay Shop</h3>
-                        <h4 className="font-Secondary text-xs font-bold">NextJS | Tailwind CSS | TypeScript</h4>
+                        <h4 className="font-Secondary text-xs font-bold">TypeScript | Solana CLI</h4>
                         <p className="font-Secondary text-sm pt-1 pb-1 lg:pr-4 leading-tight">A point-of-sale web app that accepts payments via Solana and implements a loyalty program.</p>
                         <ul className="flex space-x-3 pt-0.5">
                             <li><a href="https://github.com/edcrossfire/solana-pay-store.git" target="_blank" rel="noreferrer"><FaGithub size={20} color="#BF6330"/></a></li>
                             <li><a href="https://solana-pay-store-crossfire.vercel.app/" target="_blank" rel="noreferrer"><CgWebsite size={20} color="#BF6330"/></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="shadow-lg w-full h-40 rounded-md">
+                <div className="flex justify-between col w-full h-full">
+                    <div className="flex w-1/3 shadow-sm">
+                        <Image className="object-cover h-full rounded-l-md" src={SCREENSHOT3} alt="screenshot" />
+                    </div>
+                    <div className="flex w-2/3 pl-3 pt-2 pb-2 pr-3 flex-col justify-center text-sm h-full">
+                        <h3 className="font-Primary text-lg font-bold">Algorand Voting App</h3>
+                        <h4 className="font-Secondary text-xs font-bold">JavaScript | Python | Smart Contracts</h4>
+                        <p className="font-Secondary text-sm pt-1 pb-1 lg:pr-4 leading-tight">Web app that implements WalletConnect and keeps track of votes on the Algorand blockchain.</p>
+                        <ul className="flex space-x-3 pt-0.5">
+                            <li><a href="https://github.com/edcrossfire/algo-vote-frontend.git" target="_blank" rel="noreferrer"><FaGithub size={20} color="#BF6330"/></a></li>
+                            <li><a href="https://algo-vote-frontend.vercel.app/" target="_blank" rel="noreferrer"><CgWebsite size={20} color="#BF6330"/></a></li>
                         </ul>
                     </div>
                 </div>
--- components/Social.jsx
+++ components/Social.jsx
@@ -8,7 +8,7 @@ const Social = () => {
             <a href="mailto:hi@edcartwright.com" target="_blank" rel="noreferrer"><FaEnvelope size={25} color="#BF6330"/></a>
             <a href="https://www.linkedin.com/in/edcrossfire" target="_blank" rel="noreferrer"><FaLinkedinIn size={25} color="#BF6330"/></a>
             <a href="https://github.com/edcrossfire" target="_blank" rel="noreferrer"><FaGithub size={25} color="#BF6330"/></a>
-            <FaTelegramPlane size={25} color="#BF6330"/>
+            <a href="https://t.me/edcartwright" target="_blank" rel="noreferrer"><FaTelegramPlane size={25} color="#BF6330"/></a>
         </div>
     </div>
   )
