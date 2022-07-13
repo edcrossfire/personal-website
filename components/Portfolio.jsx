351568ccc2de3e9a6b47b9be39c04b73412a4d65
edcrossfire <ed@crossfire.network>
2022-07-13 14:04:22 -0400

    Updated portfolio items

--- components/Portfolio.jsx
+++ components/Portfolio.jsx
@@ -4,8 +4,10 @@ import SCREENSHOT1 from "../public/images/portfolio-screenshot.jpg";
 import SCREENSHOT2 from "../public/images/solanapay-shop.jpg";
 import SCREENSHOT3 from "../public/images/algo-vote-screenshot.jpg";
 import SCREENSHOT4 from "../public/images/crossfire-screenshot.jpg";
+import SCREENSHOT5 from "../public/images/shoe-store-screenshot.jpg";
 import {FaGithub} from "react-icons/fa";
 import {CgWebsite} from "react-icons/cg";
+import {RiDashboardLine} from "react-icons/ri";
 
 const Portfolio = () => {
   return (
@@ -29,6 +31,24 @@ const Portfolio = () => {
                         <p className="font-Secondary text-sm pt-1 pb-1 lg:pr-4 leading-tight">My own portfolio website that you&apos;re viewing now. Built using NextJS and designed with Tailwind CSS.</p>
                         <ul className="flex space-x-3 pt-0.5">
                             <li><a href="https://github.com/edcrossfire/" target="_blank" rel="noreferrer"><FaGithub size={20} color="#BF6330"/></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="shadow-lg w-full h-40 rounded-md">
+                <div className="flex justify-between col w-full h-full">
+                    <div className="flex w-1/3 shadow-sm">
+                        <Image className="object-cover h-full rounded-l-md" src={SCREENSHOT5} alt="screenshot" />
+                    </div>
+                    <div className="flex w-2/3 pl-3 pt-2 pb-2 pr-3 flex-col justify-center text-sm h-full">
+                        <h3 className="font-Primary text-lg font-bold">eCommerce Store</h3>
+                        <h4 className="font-Secondary text-xs font-bold">Sanity | Stripe</h4>
+                        <p className="font-Secondary text-sm pt-1 pb-1 lg:pr-4 leading-tight">A fully functioning eCommerce site that utilizes Sanity as a headless CMS and integrates with Stripe API for payments.</p>
+                        <ul className="flex space-x-3 pt-0.5">
+                            <li><a href="https://github.com/edcrossfire/ecommerce-sanity.git" target="_blank" rel="noreferrer"><FaGithub size={20} color="#BF6330"/></a></li>
+                            <li><a href="https://ecommerce-sanity-one.vercel.app/" target="_blank" rel="noreferrer"><CgWebsite size={20} color="#BF6330"/></a></li>
+                            <li><a href="https://ecommerce-shoe-store.sanity.studio/" target="_blank" rel="noreferrer"><RiDashboardLine size={20} color="#BF6330"/></a></li>
                         </ul>
                     </div>
                 </div>
@@ -61,7 +81,7 @@ const Portfolio = () => {
                         <p className="font-Secondary text-sm pt-1 pb-1 lg:pr-4 leading-tight">A point-of-sale web app that accepts payments via Solana and implements a loyalty program.</p>
                         <ul className="flex space-x-3 pt-0.5">
                             <li><a href="https://github.com/edcrossfire/solana-pay-store.git" target="_blank" rel="noreferrer"><FaGithub size={20} color="#BF6330"/></a></li>
-                            <li><a href="https://solana-pay-store-crossfire.vercel.app/" target="_blank" rel="noreferrer"><CgWebsite size={20} color="#BF6330"/></a></li>
+                            <li><a href="https://solana-pay-store-iota-orcin.vercel.app/" target="_blank" rel="noreferrer"><CgWebsite size={20} color="#BF6330"/></a></li>
                         </ul>
                     </div>
                 </div>
