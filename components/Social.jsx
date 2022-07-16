f72528a6ebc3e38dd8f3580ab738294620b8cc1e
edcrossfire <ed@crossfire.network>
2022-07-16 16:06:01 -0400

    Updated social and resume

--- components/Social.jsx
+++ components/Social.jsx
@@ -6,7 +6,7 @@ const Social = () => {
     <div>
         <div className="flex space-x-6 p-2">
             <a href="mailto:hi@edcartwright.com" target="_blank" rel="noreferrer"><FaEnvelope size={25} color="#BF6330"/></a>
-            <a href="https://www.linkedin.com/in/edcrossfire" target="_blank" rel="noreferrer"><FaLinkedinIn size={25} color="#BF6330"/></a>
+            <a href="https://www.linkedin.com/in/edcartwright1" target="_blank" rel="noreferrer"><FaLinkedinIn size={25} color="#BF6330"/></a>
             <a href="https://github.com/edcrossfire" target="_blank" rel="noreferrer"><FaGithub size={25} color="#BF6330"/></a>
             <a href="https://t.me/edcartwright" target="_blank" rel="noreferrer"><FaTelegramPlane size={25} color="#BF6330"/></a>
         </div>
